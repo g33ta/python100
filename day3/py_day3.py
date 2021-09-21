24ce02364688f1b00e632a18ef13912f0b7c2ebb
g33ta <geetaresu@outlook.com>
2021-09-21 16:16:31 -0400

    Added leap year code

--- day3/py_day3.py
+++ day3/py_day3.py
@@ -3,8 +3,9 @@ if year%4 == 0 :
     if year%100 != 0:
         print(f"{year} is leap year")
         
-    elif year%400 == 0:
-        print(f"{year} is leap year")
+    else:
+        if year%400 == 0:
+            print(f"{year} is leap year")
 
 else:
     print(f'{year} is not leap year')

80759f699726bf4f3ef9ad6d05cd4f0315a94f89
g33ta <geetaresu@outlook.com>
2021-09-21 17:18:43 -0400

    Added pizza code

---
+++ day3/Pizza_prize.py
@@ -0,0 +1,15 @@
+Small_Pizza  = 15
+Medium_Pizza = 20
+Large_Pizza = 25
+Pepperoni_for_Small_Pizza = 2
+Pepperoni_for_Medium_or_Large_Pizza = 3
+Extra_cheese_for_any_size_pizza = 1
+pizza_size = input('Which size oof pizza you want ?L or M or S')
+Extra_addings = input('Do yo want to add pepperoni? y or n'))
+
+if pizza_size == S:
+    if Extra_addings == 'y':
+        Total = Small_Pizza+Pepperoni_for_Small_Pizza+E
+        print(Total)
+    else:
+        print

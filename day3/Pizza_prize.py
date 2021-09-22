892043176463dd3ad6bc4bcdc4b187e8875fba0a
g33ta <geetaresu@outlook.com>
2021-09-22 16:17:45 -0400

    added another method program

--- day3/Pizza_prize.py
+++ day3/Pizza_prize.py
@@ -39,7 +39,28 @@ else:
     else:
         print(Medium_Pizza)
 
-"""Using function"""
+
+
+"""Reduced program length"""
+bill = 0
+
+if size == "S":
+  bill += 15
+elif size == "M":
+  bill += 20
+else:
+  bill += 25
+
+if add_pepperoni == "Y":
+  if size == "S":
+    bill += 2
+  else:
+    bill += 3
+    
+if extra_cheese == "Y":
+  bill += 1
+  
+print(f"Your final bill is: ${bill}.")
 
 
 

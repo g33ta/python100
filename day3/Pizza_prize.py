d3d523590e13581443d64055a8e655372f90b445
g33ta <geetaresu@outlook.com>
2021-09-22 16:15:57 -0400

    Edited code

--- day3/Pizza_prize.py
+++ day3/Pizza_prize.py
@@ -8,40 +8,7 @@ pizza_size = input('Which size oof pizza you want ?L or M or S?')
 Extra_addings = input('Do yo want to add pepperoni? y or n:')
 Extra_cheese = input('Do you want  to add Cheese?y or n:')
 
-# if pizza_size == 'S':
-#     if Extra_addings == "y" and Extra_cheese == "y":
-#         print(Small_Pizza+Pepperoni_for_Small_Pizza+Extra_cheese_for_any_size_pizza)
-#     elif Extra_addings == "y" and Extra_cheese == "n":
-#         print(Small_Pizza+Pepperoni_for_Small_Pizza)
-#     elif Extra_addings == "n" and Extra_cheese == "y":
-#         print(Small_Pizza+Extra_cheese_for_any_size_pizza)
-#     else:
-#         print(Small_Pizza)
-
-# elif pizza_size == 'M':
-#     if Extra_addings == "y" and Extra_cheese == "y":
-#         print(Medium_Pizza+Pepperoni_for_Small_Pizza+Extra_cheese_for_any_size_pizza)
-#     elif Extra_addings == "y" and Extra_cheese == "n":
-#         print(Medium_Pizza+Pepperoni_for_Small_Pizza)
-#     elif Extra_addings == "n" and Extra_cheese == "y":
-#         print(Medium_Pizza+Extra_cheese_for_any_size_pizza)
-#     else:
-#         print(Medium_Pizza)
-
-# else:
-#     pizza_size == 'L'
-#     if Extra_addings == "y" and Extra_cheese == "y":
-#         print(Large_Pizza+Pepperoni_for_Small_Pizza+Extra_cheese_for_any_size_pizza)
-#     elif Extra_addings == "y" and Extra_cheese == "n":
-#         print(Medium_Pizza+Pepperoni_for_Small_Pizza)
-#     elif Extra_addings == "n" and Extra_cheese == "y":
-#         print(Medium_Pizza+Extra_cheese_for_any_size_pizza)
-#     else:
-#         print(Medium_Pizza)
-
-"""Using function"""
-def Pizza_Prize(size):
-    if pizza_size == 'S':
+if pizza_size == 'S':
     if Extra_addings == "y" and Extra_cheese == "y":
         print(Small_Pizza+Pepperoni_for_Small_Pizza+Extra_cheese_for_any_size_pizza)
     elif Extra_addings == "y" and Extra_cheese == "n":
@@ -50,6 +17,50 @@ def Pizza_Prize(size):
         print(Small_Pizza+Extra_cheese_for_any_size_pizza)
     else:
         print(Small_Pizza)
+
+elif pizza_size == 'M':
+    if Extra_addings == "y" and Extra_cheese == "y":
+        print(Medium_Pizza+Pepperoni_for_Small_Pizza+Extra_cheese_for_any_size_pizza)
+    elif Extra_addings == "y" and Extra_cheese == "n":
+        print(Medium_Pizza+Pepperoni_for_Small_Pizza)
+    elif Extra_addings == "n" and Extra_cheese == "y":
+        print(Medium_Pizza+Extra_cheese_for_any_size_pizza)
+    else:
+        print(Medium_Pizza)
+
+else:
+    pizza_size == 'L'
+    if Extra_addings == "y":
+        print(Large_Pizza+Pepperoni_for_Small_Pizza+Extra_cheese_for_any_size_pizza)
+    elif Extra_addings == "y" and Extra_cheese == "n":
+        print(Medium_Pizza+Pepperoni_for_Small_Pizza)
+    elif Extra_addings == "n" and Extra_cheese == "y":
+        print(Medium_Pizza+Extra_cheese_for_any_size_pizza)
+    else:
+        print(Medium_Pizza)
+
+"""Using function"""
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
 
 
 

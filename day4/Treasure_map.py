48381ecb9b3ce4ec6232354898062bc3dda7139f
g33ta <geetaresu@outlook.com>
2021-09-27 16:21:50 -0400

    Addede code

---
+++ day4/Treasure_map.py
@@ -0,0 +1,7 @@
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+#print(f"{row1}\n{row2}\n{row3}")
+#position = input("Where do you want to put the treasure? ")
+matrix = f"{row1}\n{row2}\n{row3}"
+print(matrix[1])

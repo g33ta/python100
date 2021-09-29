caef8ee46c718c352aebd129c70cd698f02e0a36
g33ta <geetaresu@outlook.com>
2021-09-29 14:12:08 -0400

    Added code to students heights

---
+++ day5/students_heights.py
@@ -0,0 +1,12 @@
+#Finding average of students using loops
+students_heights = [180, 124, 165, 173, 189, 169, 146]
+sum = 0
+count = 0
+for student_height in students_heights:
+    sum += student_height 
+    count += 1
+result = sum/count
+#result = sum/len(students_heights)
+print(round(result))
+    
+

e1d2d342aee5bea71566e46e801be3f784fb2b08
g33ta <geetaresu@outlook.com>
2021-09-29 18:44:22 -0400

    Modified code

--- day5/fizz_buzz.py
+++ day5/fizz_buzz.py
@@ -7,3 +7,10 @@ for num in range(1,101):
         print('Buzz')
     else:
         print(num)
+
+"""Your program should print each number from 1 to 100 in turn.
+
+When the number is divisible by 3 then instead of printing the number it should print "Fizz".
+
+`When the number is divisible by 5, then instead of printing the number it should print "Buzz".` 
+  `And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"`S"""

65374f0d2872d8815734c761fc7d24536dc51a7d
g33ta <geetaresu@outlook.com>
2021-10-04 15:27:07 -0400

    Added code

---
+++ day7/Ckecking_letter.py
@@ -0,0 +1,23 @@
+"""Step 1 
+
+word_list = ["aardvark", "baboon", "camel"]
+
+TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word."""
+
+
+import random
+word_list = ["aardvark", "baboon", "camel"]
+#word = random.sample(word_list,1) #gives word with list indication
+word= random.choice(word_list) #gives just word
+print('The Random word is:',word)
+user_input = input('Enter a letter:')
+for letter in word:
+    if letter == user_input:
+        print('Right')
+    else:
+        print('Wrong')
+
